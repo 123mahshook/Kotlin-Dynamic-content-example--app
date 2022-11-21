1cecdfa66ff8d114b3c2077b7feb6f578d81740a
123mahshook <mahshookrk@gmail.com>
2022-11-21 09:24:32 +0530

    first commit

---
+++ app/src/main/java/com/mahshook/dynamiccontentexample/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.mahshook.dynamiccontentexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+/*
+val nameList:ArrayList<String> = arrayListOf("Jhone","Michael","Andrew","Dana")
+*/
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+                MainScreen( )
+
+        }
+    }
+}
+
+@Composable
+fun MainScreen(){
+    val greetingListState= remember {
+        mutableStateListOf<String>("jhone","Amanda")
+    }
+
+    val newNameStateContent= remember {
+        mutableStateOf("")
+    }
+
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+    horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        GreetingList(
+            greetingListState,
+            {   greetingListState.add(newNameStateContent.value) },
+            newNameStateContent.value,
+            {newName ->newNameStateContent.value=newName  }
+        )
+
+    }
+
+}
+
+@Composable
+fun GreetingList(nameList: List<String>,
+                 buttonClick:()->Unit,
+                 textFieldValue:String,
+                 textFieldUpdate:(newName:String)->Unit,
+
+){
+
+    for( item in nameList ){
+        Greeting(name =item )
+    }
+
+
+    TextField(value = textFieldValue, onValueChange =textFieldUpdate )
+
+    Button(onClick = buttonClick) {
+        Text(text = "Add new")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!",
+        style = MaterialTheme.typography.h4
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MainScreen()
+}
